648d4e7aa4aaeaca3caf6ac49b373812daebd99d
Surendra <surendravikram85@gmail.com>
2024-11-09 22:44:53 +0530

    implement navbar and news and news-lists

---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand text-danger" href="#">
+            New Portal
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Business
+                </a>
+              </li>
+
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Entertainment 
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  General
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Health
+                </a>
+              </li>
+
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Science 
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Sports
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Technology
+                </a>
+              </li>
+
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
